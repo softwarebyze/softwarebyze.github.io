aa30c94aad6935e37b6a9bc9184ef2188ae0e75c
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-10-03 09:30:25 -0400

    read a file

--- node-test/index.js
+++ node-test/index.js
@@ -1,1 +1,7 @@
-console.log("fun")
+const { readFile, readFileSync } = require('fs');
+// const txt = readFileSync('./hello.txt', 'utf8');
+readFile('./hello.txt', 'utf8', (err, txt) => {
+    console.log(txt);
+});
+
+console.log('do this ASAP');
