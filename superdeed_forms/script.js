99fdf9c3f5a5e6de61896f09b94b96c4ae62d13d
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-10-04 01:23:27 -0400

    changed # to . for screenshoturl query selector

--- superdeed_forms/script.js
+++ superdeed_forms/script.js
@@ -95,7 +95,7 @@ function removeLastChild() {
 
 function screenshotAndSubmit() {
     html2canvas(document.querySelector("#capture")).then(canvas => {
-        document.querySelector("#screenshoturl").value = canvas.toDataURL("image/jpeg", 1);
+        document.querySelector(".screenshoturl").value = canvas.toDataURL("image/jpeg", 1);
     });
     form = document.querySelector("form");
     form.submit();
