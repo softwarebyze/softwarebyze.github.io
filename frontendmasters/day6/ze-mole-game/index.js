039dc2039dd56608bc360ea32f07625b71bb13ee
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-10-21 10:08:55 +0300

    cleaned up show and hide functions

--- frontendmasters/day6/ze-mole-game/index.js
+++ frontendmasters/day6/ze-mole-game/index.js
@@ -1,50 +1,33 @@
-let root = document.querySelector(':root');
-
-let game = document.querySelector('.game');
-let holes = document.querySelectorAll('.hole');
-let flexes = document.querySelectorAll('.flex');
-let worm = document.querySelector('.worm');
-
-let currentState;
 let score = 0;
 
-let appear = function(holes) {
-    holes.forEach((hole) => {
-        hole.childNodes[0].src = './images/mole-hungry.png';
-        hole.childNodes[0].style.visibility = 'visible';
-        hole.childNodes[0].addEventListener('click', () => {
-            score++;
-            root.style.setProperty('--percent', `${score*10}%`);
-            console.log(score);
-            if (score === 10) {
-                game = document.querySelector('.game');
-                game.classList.add('win');
-                flexes.forEach((flex) => {flex.style.visibility = 'hidden'});
-                holes.forEach((hole) => {hole.childNodes[0].style.visibility = 'hidden'});
-                worm.style.visibility = 'hidden';
-                console.log('u won boi')
-            }
-        });
-        currentState = 'visible';
-    })
-}
-appear(holes);
-
-let disappear = function(holes) {
-    holes.forEach((hole) => {
-        hole.childNodes[0].style.visibility = 'hidden';
-    })
+function showMole(hole) {
+    holeImage = hole.childNodes[0];
+    holeImage.src = './images/mole-hungry.png';
+    holeImage.style.visibility = 'visible';
+    holeImage.addEventListener('click', () => holeClick(hole));
 }
 
-let change = function(state, holes) {
-    if (state === 'visible') {
-        disappear(holes);
-        currentState = 'hidden';
-    }
-    if (state === 'hidden') {
-        appear(holes);
-        currentState = 'visible';
+function hideMole(hole) {
+    hole.childNodes[0].style.visibility = 'hidden';
+}
+
+hole = document.querySelector('#hole3');
+showMole(hole);
+
+let holeClick = function(hole) {
+    score++;
+    let root = document.querySelector(':root');
+    root.style.setProperty('--percent', `${score*10}%`);
+    console.log(score);
+    if (score === 10) {
+        let flexes = document.querySelectorAll('.flex');
+        let game = document.querySelector('.game');
+        game.classList.add('win');
+        flexes.forEach((flex) => {flex.style.visibility = 'hidden'});
+        let holes = document.querySelectorAll('.hole');
+        holes.forEach((hole) => {holeImage.style.visibility = 'hidden'});
+        let worm = document.querySelector('.worm');
+        worm.style.visibility = 'hidden';
+        console.log('u won boi')
     }
 }
-
-// setInterval(() => change(currentState, holes), 800);
